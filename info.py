06293f6eebbf79d3895ed8c1a0c09b4df0d51a52
naidu158 <moviediscussion03@gmail.com>
2024-08-02 16:52:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@
 # Subscribe YouTube Channel For Amazing Bot @123telugfacts
 # Ask Doubt on telegram @prime3602
 
-SESSION = os.getenv('SESSION', 'Media_search')
+SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '20597576'))
 API_HASH = environ.get('API_HASH', '9d3a1fbae5b3368cf8c62008d8a41e7d')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
